07021ac2ee36b37c1ab6cad301a6d6223d3b95fe
Mikel-UA <101286435+Mikel-UA@users.noreply.github.com>
2023-10-11 06:01:48 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+# Linkedin user and password
+username="XXXX"
+password="XXXX"
+
+# Users to contact = number of recruiters to send invitation
+users_to_contact=0
+
+# Text to type in the linkedin search box
+search_box="recruiter meta"
+
+context="""
+My name is Mikel Uranga Ahijado.
+I live in Madrid.
+I'm a native Spanish speaker.
+I speak english, i'm bilingual. 
+I have the Cambridge B2 certificate in english.
+I'm currently studying German.
+I have worked with agile technologies 
+Im interested in working with Artificial Intelligence.
+I have a medium level of knowledge in Databases.
+I have a good level of PL/SQL, MySQL and NoSQL.
+I have a good programming level with libraries like Pandas, NumPy, Selenium, BeautifulSoup 
+I code in Python. I have a good level of Python. I have been using Python for a few years.
+I would like to work with Python on Artificial Intelligence or Big Data.
+"""
+
+connectmessage="""
+    Hello! My name is Mikel Uranga and this is an automated message sent by a bot that implements AI, both the Bot and the AI have been designed by me in Python as my final thesis.
+    
+    If you type "more info" in the chat, the bot will give you information on how to use it.
+    
+    Sorry for the inconvenience.
+"""
